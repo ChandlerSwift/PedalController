fb267b6e841a770eeebf3514a2954781187b3c5c
Chandler Swift <chandler@chandlerswift.com>
2017-12-12 15:19:48 -0600

    Print MIDI changes over serial

--- PedalController.ino
+++ PedalController.ino
@@ -6,6 +6,8 @@ const int LOAD = 2;
 const int CLOCK = 3;
 const int DATA = 4;
 
+int octave = 2; // TODO: octave adjustment using buttons?
+
 bool pedals[NUM_PEDALS];
 
 void setup() {
@@ -32,17 +34,19 @@ void loop() {
   {
     digitalWrite(CLOCK, 0);
     delayMicroseconds(1000); // TODO: reduce?
-    Serial.print(digitalRead(DATA));
-    /*if (digitalRead(DATA) != pedals[pedal]) // key has been pressed or released
+    if (digitalRead(DATA) != pedals[pedal]) // key has been pressed or released
     { 
-      if (digitalRead(DATA))
-        usbMIDI.sendNoteOn(key, 100, manual);
-      else
-        usbMIDI.sendNoteOff(key, 100, manual);
+      if (digitalRead(DATA)) {
+        Serial.print("KEY "); Serial.print(pedal + 36); Serial.print(" pressed");
+        //usbMIDI.sendNoteOn(key, 100, manual);
+      } else {
+        Serial.print("KEY "); Serial.print(pedal + 36); Serial.print(" released");
+        //usbMIDI.sendNoteOff(key, 100, manual);
+      }
       pedals[pedal] = digitalRead(DATA); // Update new value to array
-    }*/
+    }
     digitalWrite(CLOCK, 1);
     delayMicroseconds(1000); // TODO: reduce?
   }
-  delay(100);
+  //delay(100);
 }
